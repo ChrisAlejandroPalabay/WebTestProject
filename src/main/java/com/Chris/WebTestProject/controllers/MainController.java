2467dc8e938f3782eb8b7c2f8826ddd9d1025541
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-09-22 11:14:10 +0800

    Thymeleaf Loops

--- src/main/java/com/Chris/WebTestProject/controllers/MainController.java
+++ src/main/java/com/Chris/WebTestProject/controllers/MainController.java
@@ -10,23 +10,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String homePage(){
+    public String homePage() {
         return "car";
     }
 
 
     @RequestMapping("/car")
-    public ModelAndView carForm(@ModelAttribute Car car, ModelAndView mv){
+    public ModelAndView carForm(@ModelAttribute Car car, ModelAndView mv) {
         mv.setViewName("carResult");
-        mv.addObject("car",car);
+        mv.addObject("cars");
         return mv;
     }
 
-
-
-
+    @RequestMapping("/list")
+    public Model list(Model mv) {
+        Car[] car = {
+                new Car("toyota", "red", 2030),
+                new Car("Honda", "red", 2030),
+                new Car("Ford", "red", 2030)};
+        mv.addAttribute("ListOfCars",car);
+        return mv;
+    }
 }
