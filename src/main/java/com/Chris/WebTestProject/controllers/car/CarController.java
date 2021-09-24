01b9f4d62066b47def468c18c35ed6fe251d89ee
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-09-24 14:08:22 +0800

    Exploring Annotations

--- src/main/java/com/Chris/WebTestProject/controllers/car/CarController.java
+++ src/main/java/com/Chris/WebTestProject/controllers/car/CarController.java
@@ -3,6 +3,8 @@ package com.Chris.WebTestProject.controllers.car;
 
 import com.Chris.WebTestProject.dao.GetServices;
 import com.Chris.WebTestProject.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,19 +12,27 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/get")
+@RequestMapping("/rest")
 public class CarController {
 
-    GetServices services = new GetServices();
+    @Autowired
+    GetServices services;
 
     @GetMapping("/list")
-    public List<Car> carList(){
-       return services.carList();
+    public List<Car> getList(){
+        return services.getAllCars();
     }
 
     @GetMapping("/getById/{id}")
     public List<Car> getById(@PathVariable int id){
-        return services.getCarById(id);
+        return services.getById(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public String addCar(@RequestBody Car car){
+        System.out.println(services.getAllCars());
+        return services.addCar(car);
     }
 
 }
--- src/main/java/com/Chris/WebTestProject/dao/GetServices.java
+++ src/main/java/com/Chris/WebTestProject/dao/GetServices.java
@@ -1,22 +1,35 @@
 package com.Chris.WebTestProject.dao;
 
 import com.Chris.WebTestProject.model.Car;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class GetServices {
 
-    public List<Car> carList(){
-        List listOfCars = List.of(
-                new Car(1,"Toyota","Blue",2000),
-                new Car(2,"Hyundai","Blue",2000),
-                new Car(3,"Honda","Red",2000)
-        );
-        return listOfCars;
+    public List<Car> loadCars(){
+        List<Car> carList = new ArrayList<Car>();
+        carList.add(new Car(1,"Toyota","Blue",2020));
+        carList.add(new Car(2,"Honda","Red",2021));
+        return carList;
     }
 
-    public List<Car> getCarById(int id){
-        return carList().stream().filter(car -> car.getId() == id).collect(Collectors.toList());
+    private List<Car> cars = loadCars();
+
+    public List<Car> getAllCars(){
+        return cars;
+    }
+
+    public List<Car> getById(int id){
+        return getAllCars().stream().filter(car -> car.getId() == id).collect(Collectors.toList());
+    }
+
+
+    public String addCar(Car car){
+        cars.add(car);
+        return "Add success!";
     }
 }
